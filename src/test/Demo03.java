fdef6a7c861c8318272356d77771b1ef9657eaed
Neo <kwaad2009@163.com>
2019-09-12 17:31:41 +0800

    20190912-002 Demo03 test volatile

--- src/test/Demo03.java
+++ src/test/Demo03.java
@@ -1,10 +1,46 @@
 package test;
 
+import java.util.concurrent.TimeUnit;
+/**
+ * volatile test
+ * @author Administrator
+ *
+ */
 public class Demo03 {
-
+	
+	public volatile boolean flag = false;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Demo03 d = new Demo03();
+		
+		Thread t1 = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				
+				for(int i=1;i<=10;i++) {
+					System.out.println(i);
+					try {
+						TimeUnit.SECONDS.sleep(1);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				System.out.println(Thread.currentThread().getName()+"已经执行完毕！");
+				d.flag = true;
+			}
+		});
+		
+		Thread t2 = new Thread(()-> {
+			while(!d.flag) {
+				//自旋
+			}
+			System.out.println(Thread.currentThread().getName()+"开始执行！");
+		});
+		
+		t1.start();
+		t2.start();
 	}
 
 }
